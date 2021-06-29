fd7152f5014036ca272ee1a01e6b7770c9333a16
lxthyme <1094426094@qq.com>
2021-06-29 10:05:03 +0800

    update

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-import "katex/dist/katex.min.css"
+// import "katex/dist/katex.min.css"
 
 export default function Template({ data }) {
   const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
+  console.log('----->>>frontmatter: ', frontmatter)
+  console.log('----->>>HTML: ', html)
   return (
     <div className="blog-post-container">
       <div className="blog-post">
